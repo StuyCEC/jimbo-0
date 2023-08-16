ab8b628fa63731ffdd4e0140260551a50bb6f2ab
Kellen Yu <thanoscarthanoscar420420@gmail.com>
2023-08-15 22:56:26 -0400

    started emulator

---
+++ emulator/main.py
@@ -0,0 +1,55 @@
+class CPU():
+    def __init__(self, rompath: str, debug: bool):
+        #Registers
+        self.A = 0
+        self.B = 0
+        self.C = 0
+        self.M = 0
+
+        #Flags
+        self.ZERO = False
+        self.CARRY = False
+        self.BORROW = False
+
+        #Instruction Register
+        self.IX = 0
+        self.IY = 0
+        self.IZ = 0
+
+        #ALU Registers
+        self.ALU_A = 0
+        self.ALU_B = 0
+
+        #Program Counter
+        self.PC = 0 
+
+        #Memory
+        self.ROM = [0] * 1024
+        self.RAM = [0] * 1006
+        
+        #Stack Adresses
+        self.SPL = 0xFEE
+        self.SPH = 0xFEF
+
+    def readmem(self, address) -> int:
+        if (address <= 0x3FF):
+            return self.ROM[address]
+        elif ((address >= 0x400) and (address <= 0xFFF)):
+            return self.RAM[address]
+        else: 
+            return 0
+
+    def fetch_instruction(self) -> None:
+        byte = self.readmem(self.PC)
+        self.IX = int(format(byte, '#010b')[2:7], 2)
+        self.IY = int(format(byte, '#010b')[7:9], 2)
+        self.IZ = int(format(byte, '#010b')[9], 2)
+        self.PC += 1
+        self.PC %= 0xFFF
+
+
+if __name__ == "__main__":
+    pass
+        
+
+
