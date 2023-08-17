5c87037b1bdf11857fe71a2497c936fc3851398f
Kellen Yu <thanoscarthanoscar420420@gmail.com>
2023-08-16 23:41:18 -0400

    emulator changes

--- emulator/main.py
+++ emulator/main.py
@@ -28,6 +28,7 @@ class CPU():
         self.SPL = 0xFEE
         self.SPH = 0xFEF
 
+
     def readmem(self, address) -> int:
         if (address <= 0x3FF):
             return self.ROM[address]
@@ -44,10 +45,41 @@ class CPU():
         self.PC += 1
         self.PC %= 0xFFF
 
-    def check_reg(self, register: int) -> bool:
-        if reg > -1 and reg < 4:
+    def check_reg(self, r1: int) -> bool:
+        if ((r1> -1) and (r1< 4)):
             return True
         return False
+
+    def mov(self, r1, mode) -> None:
+        check_reg = self.check_reg
+        readmem = self.readmem
+        PC = self.PC
+        REG = self.REG
+
+        if (self.check_reg(r1)):
+            if (mode == 0):
+                r2 = readmem(PC)
+                if (check_reg(r2)):
+                    REG[r1] = REG[r2]
+
+            elif (mode == 1):
+                REG[r1] = readmem(PC)
+
+            else:
+                print("INVALID Z VALUE")
+                return
+
+            PC += 1
+            PC %= 0xFFF
+
+        else:
+            print("INVALID REG (R1)")
+
+
+
+
+
+
 
 if __name__ == "__main__":
     pass
